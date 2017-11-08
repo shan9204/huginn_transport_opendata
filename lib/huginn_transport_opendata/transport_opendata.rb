8311c1b2fbc3cf9be216a8f0ad2483db957b335a
Shanmathuran Sritharan <shanmathuran.sritharan@xovis.com>
2017-11-08 17:43:37 +0100

    create events

--- lib/huginn_transport_opendata/transport_opendata.rb
+++ lib/huginn_transport_opendata/transport_opendata.rb
@@ -6,7 +6,7 @@ module Agents
     cannot_receive_events!
     can_dry_run!
 
-    gem_dependency_check { defined?(Transprt::Client)}
+    gem_dependency_check {defined?(Transprt::Client)}
 
     description <<-MD
       Add a Agent description here
@@ -30,21 +30,28 @@ module Agents
       # received_event_without_error?
     end
 
-#    def check
-#    end
+    def check
+      item["from"] = interpolated["from"] if interpolated["from"].present?
+      item["to"] = interpolated["to"] if interpolated["to"].present?
 
-    def receive(incoming_events)
-      incoming_events.each do |event|
-          interpolate_with(event) do
-            item["from"] = interpolated["from"] if interpolated["from"].present?
-            item["to"] = interpolated["to"] if interpolated["to"].present?
-
-            log "sending request item: #{item}"
-            transprt = Transprt::Client.new
-            result = transprt.connections from: item["from"], to: item["to"]
-            create_event(result)
-          end
-      end
+      log "sending request item: #{item}"
+      transprt = Transprt::Client.new
+      result = transprt.connections from: item["from"], to: item["to"]
+      create_event(result)
     end
+
+    # def receive(incoming_events)
+    #   incoming_events.each do |event|
+    #       interpolate_with(event) do
+    #         item["from"] = interpolated["from"] if interpolated["from"].present?
+    #         item["to"] = interpolated["to"] if interpolated["to"].present?
+    #
+    #         log "sending request item: #{item}"
+    #         transprt = Transprt::Client.new
+    #         result = transprt.connections from: item["from"], to: item["to"]
+    #         create_event(result)
+    #       end
+    #   end
+    # end
   end
 end
