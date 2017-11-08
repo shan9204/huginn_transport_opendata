182e908225ddf52be392a19645014bf7ea88afda
Shanmathuran Sritharan <shanmathuran.sritharan@xovis.com>
2017-11-08 08:38:55 +0100

    use transprt

--- lib/huginn_transport_opendata/transport_opendata.rb
+++ lib/huginn_transport_opendata/transport_opendata.rb
@@ -1,6 +1,9 @@
 module Agents
   class TransportOpendata < Agent
-    default_schedule '12h'
+
+    cannot_be_scheduled!
+
+    gem_dependency_check { defined?(Transprt::Client)}
 
     description <<-MD
       Add a Agent description here
@@ -8,8 +11,19 @@ module Agents
 
     def default_options
       {
+          "from" => "{{ from }}",
+          "to" => "{{ to }}",
+          "date" => "{{ date }}",
+          "time" => "{{ time }}",
+          "isArrivalTime" => "0"
       }
     end
+
+    form_configurable :from
+    form_configurable :to
+    form_configurable :date
+    form_configurable :time
+    form_configurable :isArrivalTime, type: :int
 
     def validate_options
     end
@@ -18,13 +32,25 @@ module Agents
       # Implement me! Maybe one of these next two lines would be a good fit?
       # checked_without_error?
       # received_event_without_error?
-      log "From transport #{Transprt::Client.new.locations query: 'Geneva'}"
     end
 
 #    def check
 #    end
 
-#    def receive(incoming_events)
-#    end
+    def receive(incoming_events)
+      incoming_events.each do |event|
+          interpolate_with(event) do
+            item["from"] = interpolated["from"] if interpolated["from"].present?
+            item["to"] = interpolated["to"] if interpolated["to"].present?
+            item["date"] = interpolated["date"] if interpolated["date"].present?
+            item["time"] = interpolated["time"] if interpolated["time"].present?
+            item["isArrivalTime"] = interpolated["isArrivalTime"].to_i if interpolated["isArrivalTime"].present?
+
+            log "sending request item: #{item}"
+            transprt = Transprt::Client.new
+            transprt.connections from: item["from"], to: item["to"]
+          end
+      end
+    end
   end
 end
