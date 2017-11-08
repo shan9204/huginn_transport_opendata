28ba9ae24729ceb01555dd2d2bfa4beb63286881
Shanmathuran Sritharan <shanmathuran.sritharan@xovis.com>
2017-11-08 17:27:14 +0100

    add events option

--- lib/huginn_transport_opendata/transport_opendata.rb
+++ lib/huginn_transport_opendata/transport_opendata.rb
@@ -14,19 +14,12 @@ module Agents
 
     def default_options
       {
-          "from" => "",
-          "to" => "",
-          "date" => "",
-          "time" => "",
-          "isArrivalTime" => 0
+
       }
     end
 
     form_configurable :from
     form_configurable :to
-    form_configurable :date
-    form_configurable :time
-    form_configurable :isArrivalTime, type: :int
 
     def validate_options
     end
@@ -45,14 +38,11 @@ module Agents
           interpolate_with(event) do
             item["from"] = interpolated["from"] if interpolated["from"].present?
             item["to"] = interpolated["to"] if interpolated["to"].present?
-            item["date"] = interpolated["date"] if interpolated["date"].present?
-            item["time"] = interpolated["time"] if interpolated["time"].present?
-            item["isArrivalTime"] = interpolated["isArrivalTime"].to_i if interpolated["isArrivalTime"].present?
 
             log "sending request item: #{item}"
             transprt = Transprt::Client.new
             result = transprt.connections from: item["from"], to: item["to"]
-            create_event :payload => result
+            create_event(payload:result)
           end
       end
     end
