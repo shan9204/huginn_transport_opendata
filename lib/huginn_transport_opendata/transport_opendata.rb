7452bfbff06c3d5c2f41853f22d79f07d78e87c6
Shanmathuran Sritharan <shanmathuran.sritharan@xovis.com>
2017-11-08 08:21:54 +0100

    use transprt

--- lib/huginn_transport_opendata/transport_opendata.rb
+++ lib/huginn_transport_opendata/transport_opendata.rb
@@ -12,13 +12,13 @@ module Agents
     end
 
     def validate_options
-      log "From transport #{Transprt::Client.new.locations query: 'Geneva'}"
     end
 
     def working?
       # Implement me! Maybe one of these next two lines would be a good fit?
       # checked_without_error?
       # received_event_without_error?
+      log "From transport #{Transprt::Client.new.locations query: 'Geneva'}"
     end
 
 #    def check
