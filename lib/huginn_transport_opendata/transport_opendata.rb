0b63ee61c37651d6613a72bac3835af691c62cb9
Shanmathuran Sritharan <shanmathuran.sritharan@xovis.com>
2017-11-20 23:00:47 +0100

    use output json for event

--- lib/huginn_transport_opendata/transport_opendata.rb
+++ lib/huginn_transport_opendata/transport_opendata.rb
@@ -38,7 +38,8 @@ module Agents
 
       log "received result: #{result}"
 
-      create_event payload: JSON.parse("[#{result}]")
+      result.each do |item|
+        create_event payload: item
     end
   end
 end
